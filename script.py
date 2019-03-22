f272b84141154901627ebbd466b238dea6627030
migrulos <migrulos@gmail.com>
2019-03-22 02:10:03 -0700

    try to break the build

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ import unittest
 class NumbersTest(unittest.TestCase):
 
     def test_equal(self):
-        self.assertEqual(2 + 2, 5)
+        self.assertEqual(2 + 2, 6)
 
 if __name__ == '__main__':
     unittest.main()
